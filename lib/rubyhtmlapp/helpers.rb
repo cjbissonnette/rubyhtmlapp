c5e66be77bb4b040c2efd338e17d5489c036d95c
Curtis Bissonnette <cbissonnette@innovative-ss.com>
2013-05-15 13:50:49 -0400

    add better file matching so emacs artifacts are not included

--- lib/rubyhtmlapp/helpers.rb
+++ lib/rubyhtmlapp/helpers.rb
@@ -5,12 +5,12 @@ require 'find'
 module RubyHtmlApp
   module Helpers
     def render(partial, locals={}, &block)
-      Tilt.new(find_f(@partial_path, "_#{partial}.html*")).render(self, locals, &block)
+      Tilt.new(find_f(@partial_path, /_#{partial}.html[a-z\.0-9]*$/i)).render(self, locals, &block)
     end
     
     def include_dir(dir, locals={}, &block)
       html = ""
-      find_f_each(File.join(@partial_path, dir), "*.html*") do |f|
+      find_f_each(File.join(@partial_path, dir), /[0-9a-z]{1}\.html[a-z\.0-9]*$/i) do |f|
         html.concat(Tilt.new(f).render(self, locals, &block))
       end
       return html
@@ -23,14 +23,14 @@ module RubyHtmlApp
 
     def find_f(path, pattern)
       Find.find(path) do |f|
-        return f if File.fnmatch?(pattern, File.basename(f))
+        return f if File.basename(f).match(pattern)
       end
       nil
     end
     
     def find_f_each(path, pattern)
       Find.find(path) do |f|
-        yield(f) if File.fnmatch?(pattern, File.basename(f))
+        yield(f) if File.basename(f).match(pattern)
       end
     end
   end
